7c641bd77649e4db5dca83cb598c78104c97c545
Randy Taylor <tehgecKozzz@gmail.com>
2018-09-18 16:34:32 -0400

    Cleanup whitespace

--- src/test/java/core/CardTest.java
+++ src/test/java/core/CardTest.java
@@ -9,7 +9,7 @@ public class CardTest extends TestCase {
 		Card c1 = new Card('S', "10");
 		Card c2 = new Card('S', "10");
 		Card c3 = new Card('D', "J");
-		
+
 		assertEquals(c1, c1);
 		assertEquals(c1, c2);
 		assertNotEquals(c1, c3);
