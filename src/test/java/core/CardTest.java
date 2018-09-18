4be83b707c8a41449bc147387560160157de3036
Randy Taylor <tehgecKozzz@gmail.com>
2018-09-18 15:42:56 -0400

    [test] Add CardTest class and implement testEquals()

---
+++ src/test/java/core/CardTest.java
@@ -0,0 +1,17 @@
+package core;
+
+import static org.junit.Assert.assertNotEquals;
+
+import junit.framework.TestCase;
+
+public class CardTest extends TestCase {
+	public void testEquals() {
+		Card c1 = new Card('S', "10");
+		Card c2 = new Card('S', "10");
+		Card c3 = new Card('D', "J");
+		
+		assertEquals(c1, c1);
+		assertEquals(c1, c2);
+		assertNotEquals(c1, c3);
+	}
+}
