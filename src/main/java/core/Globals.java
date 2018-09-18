7d1d014afe7fe331f1c708f230b742170e8cac0b
Randy Taylor <tehgecKozzz@gmail.com>
2018-09-18 14:49:47 -0400

    Add Globals.java

---
+++ src/main/java/core/Globals.java
@@ -0,0 +1,6 @@
+package core;
+
+public class Globals {
+	public static final char[] suites = {'S', 'C', 'D', 'H'};
+	public static final String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+}
