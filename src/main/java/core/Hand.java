ba18060bde82810bf4e7650783d13b6f6f22edef
Randy Taylor <tehgecKozzz@gmail.com>
2018-09-18 17:23:12 -0400

    Add Hand class and implement ctor, getHand(), getTotal(), and add()

---
+++ src/main/java/core/Hand.java
@@ -0,0 +1,57 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+	private List<Card> hand;
+
+	Hand() {
+		this.hand = new ArrayList<>();
+	}
+
+	public List<Card> getHand() {
+		return this.hand;
+	}
+
+	public int getTotal() {
+		int total = 0;
+		List<Card> aces = new ArrayList<>(); 
+
+		for (Card card : this.hand) {
+			String rank = card.getRank();
+
+			// 2 - 10.
+			if (rank.chars().allMatch(Character::isDigit)) {
+				total += Integer.parseInt(rank);
+			// Jack, Queen, King.
+			} else if (rank == "J" || rank == "Q" || rank == "K" ) {
+				total += 10;
+			// Ace.
+			} else if (rank == "A") {
+				// We will deal with aces after everything else has been calculated.
+				aces.add(card);
+			}
+		}
+
+		// We have 2 aces, but setting one to 11 and the other to 1 busts,
+		// so they both need to count as 1 each.
+		if (aces.size() > 1 && total + 12 > 21) {
+			total += 2;
+		} else {
+			for (int i = 0; i < aces.size(); i++) {
+				if (total + 11 > 21) {
+					total += 1;
+				} else {
+					total += 11;
+				}
+			}
+		}
+
+		return total;
+	}
+
+	public void addCard(Card card) {
+		this.hand.add(card);
+	}
+}
